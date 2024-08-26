943c310972364334251a0fe61667628cb78fac56
akin <akin.isletme@gmail.com>
2024-08-26 11:40:12 +0200

    Implement basic math function in script nd console Create main functionality of calculator

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("let's do the math, boy.")
+
+
+let a = 20;
+let b = 4;
+
+// Add two integers together
+
+function add(a, b) {
+    console.log((a + b));
+};
+
+add(a, b);
+
+// Subtract one int from another
+
+function subtract (a, b) {
+    console.log(a - b);
+};
+
+subtract(a, b);
+
+// Multiply to ints
+
+function multiply (a, b) {
+    console.log(a * b)
+};
+
+multiply(a, b);
+
+// Divide one int through another
+
+function divide (a, b) {
+    console.log(a / b)
+};
+
+divide(a, b);
